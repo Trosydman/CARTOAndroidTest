4725ea3397dcd615cf617dca8793ed5e260ede36
Carlos Mesquita <cmesquita@justaddmusic.net>
2021-02-19 20:26:39 +0100

    Prepare MapFragment.kt and fragment_map.xml
    * Prepare UI skeleton inside the MapFragment.kt
    * Implement layout inside the fragment_map.xml
    * Add drawable resources

--- app/src/main/java/com/carto/androidtest/ui/map/MapFragment.kt
+++ app/src/main/java/com/carto/androidtest/ui/map/MapFragment.kt
@@ -1,24 +1,59 @@
 package com.carto.androidtest.ui.map
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.carto.androidtest.R
 import com.carto.androidtest.databinding.FragmentMapBinding
 import com.carto.androidtest.ui.MainViewModel
-import com.google.android.gms.maps.CameraUpdateFactory
+import com.carto.androidtest.ui.custom.PoiDetailsBottomSheet
+import com.carto.androidtest.ui.custom.RouteDetailsView
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import dagger.hilt.android.AndroidEntryPoint
 
+const val MAP_MIN_ZOOM = 6.5f
+const val CAMERA_BOUNDS_PADDING = 60
+
 @AndroidEntryPoint
-class MapFragment : Fragment() {
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback, PoiDetailsBottomSheet.OnClickListener,
+    RouteDetailsView.OnClickListener, GoogleMap.OnMarkerClickListener,
+    GoogleMap.OnMyLocationButtonClickListener {
+
+    enum class MapZoomLevel(val level: Float) {
+        WORLD(1f),
+        CONTINENT(5f),
+        CITY(10f),
+        STREETS(15f),
+        BUILDINGS(20f)
+    }
+
+    private val onPoiDetailsVisibilityChangedListener =
+        object : PoiDetailsBottomSheet.OnVisibilityChangedListener {
+            override fun onShow() {
+                // TODO
+            }
+
+            override fun onHide() {
+                // TODO
+            }
+
+        }
+
+    private val onRouteDetailsVisibilityChangedListener =
+        object : RouteDetailsView.OnVisibilityChangedListener {
+            override fun onShow() {
+                // TODO
+            }
+
+            override fun onHide() {
+                // TODO
+            }
+
+        }
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -26,37 +61,73 @@ class MapFragment : Fragment() {
         get() = _binding!!
     private var _binding: FragmentMapBinding? = null
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        val fakeCurrentLocation = LatLng(-33.3000802,149.0913524)
-        val blueDotIcon = BitmapDescriptorFactory.fromResource(R.drawable.ic_bluedot)
+    private lateinit var map: GoogleMap
+    private var currentLocationMarker: MarkerOptions? = null
+    private var poiDetailsSheet: PoiDetailsBottomSheet? = null
 
-        val currentLocationMarker = MarkerOptions()
-            .position(fakeCurrentLocation)
-            .icon(blueDotIcon)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        googleMap.addMarker(currentLocationMarker)
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocationMarker.position, 5f))
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMapBinding.inflate(inflater, container, false)
-
-        initializeMap()
-
-        return binding.root
-    }
-
-    private fun initializeMap() {
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        _binding = FragmentMapBinding.bind(view)
+        initMap()
+        initViews()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
 
         _binding = null
+        poiDetailsSheet = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.setOnMarkerClickListener(this)
+        map.setOnMyLocationButtonClickListener(this)
+        // TODO
+    }
+
+    override fun onMarkerClick(marker: Marker?): Boolean {
+        // TODO
+        return false
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        // TODO
+        return false
+    }
+
+    override fun onDirectionsFabClicked() {
+        // TODO
+    }
+
+    override fun onCloseButtonClicked() {
+        // TODO
+    }
+
+    private fun initMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    private fun initViews() {
+        with(binding) {
+            poiDetailsSheet = PoiDetailsBottomSheet(poiDetailsBottomSheet.root).apply {
+                onVisibilityChangedListener = onPoiDetailsVisibilityChangedListener
+                onClickListener = this@MapFragment
+            }
+
+            currentLocationFAB.setOnClickListener {
+                // TODO
+            }
+
+            searchButton.setOnClickListener {
+                // TODO
+            }
+
+            helpButton.setOnClickListener {
+                // TODO
+            }
+        }
     }
 }
