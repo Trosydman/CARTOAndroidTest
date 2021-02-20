d95b26a9845bfcbca053d36d05278a1e64a7fbff
Carlos Mesquita <cmesquita@justaddmusic.net>
2021-02-20 18:02:24 +0100

    Extend Poi model to provide the direction image

--- app/src/main/java/com/carto/androidtest/domain/model/Poi.kt
+++ app/src/main/java/com/carto/androidtest/domain/model/Poi.kt
@@ -1,5 +1,9 @@
 package com.carto.androidtest.domain.model
 
+import androidx.annotation.DrawableRes
+import com.carto.androidtest.R
+import java.util.*
+
 data class Poi(
     val id: String,
     val title: String,
@@ -9,4 +13,19 @@ data class Poi(
     val image: String,
     val longitude: Double,
     val latitude: Double
-)
+) {
+
+    @DrawableRes
+    val directionImage: Int =
+        when (direction.toUpperCase(Locale.ROOT)) {
+            "N" -> R.drawable.direction_n
+            "N-E" -> R.drawable.direction_ne
+            "E" -> R.drawable.direction_e
+            "S-E" -> R.drawable.direction_se
+            "S" -> R.drawable.direction_s
+            "S-W" -> R.drawable.direction_sw
+            "W" -> R.drawable.direction_w
+            "N-W" -> R.drawable.direction_nw
+            else -> R.drawable.direction_unknown
+        }
+}
